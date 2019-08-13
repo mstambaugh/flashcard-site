edeb5ef439fa14d96e646a30e5c9742c3ddd230b
Student <example@example.com>
2019-08-13 15:36:08 -0700

    create JS toggle functions for some flashcards, add div class for columns

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $("#clickable").click(function() {
+    $("#javascript-info").toggle();
+  });
+  $("#clickable2").click(function() {
+    $("#operators-info").toggle();
+  });
+});
